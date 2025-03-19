fc7e3aec869676d3c867ef977f6b3c6d52e0d672
MuhammadHassan <99420700+itshasu@users.noreply.github.com>
2025-03-20 01:16:19 +0500

    Front-End

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './DescriptionBox.css';
+
+const DescriptionBox = () => {
+    return (
+        <div className='descriptionbox'>
+            <div className="descriptionbox-navigator">
+                <div className="descriptionbox-nav-box">Description</div>
+                <div className="descriptionbox-nav-box fade">Reviews</div>
+            </div>
+            <div className="descriptionbox-description">
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Culpa quia accusamus eligendi quasi qui illo molestias consequatur iure adipisci modi, eveniet minus ad, non repudiandae dolor nobis, aut ut numquam.
+                    Consequatur ratione necessitatibus, cumque recusandae eaque incidunt nemo rem, nulla libero doloremque enim? Repudiandae, cupiditate sequi quidem necessitatibus odio sed temporibus corporis similique aperiam ea doloremque distinctio. Repudiandae, saepe totam!
+                    Ab nisi eveniet mollitia in vel officia ex repudiandae, aperiam placeat corrupti nihil reprehenderit eligendi vero. Deserunt soluta accusantium, nihil enim officiis accusamus odit, id quis nostrum dolorem voluptates unde!
+                    Architecto nemo necessitatibus nam temporibus vero consequatur veniam minima eos repellat repudiandae! Ut accusantium incidunt reprehenderit error ratione assumenda voluptate pariatur exercitationem sequi. Quibusdam vel exercitationem reiciendis nemo, iste aliquid?
+                    Nihil ab doloremque non, quibusdam rem, excepturi sequi et laudantium ea molestias maxime aliquam exercitationem voluptatem nemo error omnis cumque numquam dolorum nam. Inventore excepturi, tempora nihil placeat libero in.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, est ab? Iste, at consequuntur debitis ullam modi quos culpa similique reiciendis non omnis esse. Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, est ab? Iste, at consequuntur debitis ullam modi quos culpa similique reiciendis non omnis esse.</p>
+            </div>
+        </div>
+    )
+};
+
+export default DescriptionBox;
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,43 @@
+import React, { useContext, useRef, useState } from 'react';
+import './Navbar.css';
+
+import logo from '../Assets/logo.png';
+import cart_icon from '../Assets/cart_icon.png';
+import { Link } from 'react-router-dom';
+import { ShopContext } from '../../Context/ShopContext';
+import arrow_icon from '../Assets/breadcrum_arrow.png';
+
+const Navbar = () => {
+
+    const [menu, setMenu] = useState("shop");
+    const { getTotalCartItems } = useContext(ShopContext);
+    const menuRef = useRef();
+
+    const dropdown_toggle = (e) => {
+        menuRef.current.classList.toggle('nav-menu-visible');
+        e.target.classList.toggle('open');
+    }
+
+    return (
+        <div className='navbar'>
+            <div className="nav-logo">
+                <img src={logo} alt="" />
+                <p>SHOOPER</p>
+            </div>
+            <img className='nav-dropdown' onClick={dropdown_toggle} src={arrow_icon} alt="" />
+            <ul ref={menuRef} className='nav-menu'>
+                <li onClick={() => { setMenu("shop") }}><Link style={{ textDecoration: 'none' }} to='/'>Shop</Link>{menu === "shop" ? <hr /> : <></>}</li>
+                <li onClick={() => { setMenu("mens") }}><Link style={{ textDecoration: 'none' }} to='/mens'>Men</Link>{menu === "mens" ? <hr /> : <></>}</li>
+                <li onClick={() => { setMenu("women") }}><Link style={{ textDecoration: 'none' }} to='women'>Women</Link>{menu === "women" ? <hr /> : <></>}</li>
+                <li onClick={() => { setMenu("kids") }}><Link style={{ textDecoration: 'none' }} to='/kids'>Kids</Link>{menu === "kids" ? <hr /> : <></>}</li>
+            </ul>
+            <div className="nav-login-cart">
+                <Link to='/Login'><button>Login</button></Link>
+                <Link to='/Cart'><img src={cart_icon} alt="" /></Link>
+                <div className="nav-cart-count">{getTotalCartItems()}</div>
+            </div>
+        </div>
+    )
+};
+
+export default Navbar;
